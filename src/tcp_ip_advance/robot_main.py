d2d2dd2eeafea40311d9c2b299d4e34477bf8f5a
RINGULARITTY <63746619+RINGULARITTY@users.noreply.github.com>
2023-08-28 13:38:01 +0200

    Fix offset and top

--- src/tcp_ip_advance/robot_main.py
+++ src/tcp_ip_advance/robot_main.py
@@ -42,11 +42,11 @@ while True:
     elif msg[0] == "gotoj":
         computer.gotoj(msg[1:7], *msg[7:])
     elif msg[0] == "gotoc":
-        computer.gotoc(msg[1:7], msg[7:13], msg[13], msg[14], msg[15], msg[16], msg[17])
+        computer.gotoc(msg[1:7], msg[7:13], *msg[13:])
     elif msg[0] == "gotooffset":
-        computer.gotooffset(msg[1:7], msg[7], msg[8], msg[9], msg[10])
+        computer.gotooffset(msg[1:7], *msg[7:])
     elif msg[0] == "gotop":
-        computer.gotop(msg[1:7], msg[17], msg[8], msg[9], msg[10])
+        computer.gotop(msg[1:7], *msg[7:])
     elif msg[0] == "get_current_posj":
         computer.get_posj()
     elif msg[0] == "get_current_rotm":
